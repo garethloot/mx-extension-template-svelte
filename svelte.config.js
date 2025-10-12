740b1ba2fb1e4e85e506553ff713c27234b341a6
garethloot <32203526+garethloot@users.noreply.github.com>
2025-10-12 18:17:03 +0200

    add async to svelte components

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+  compilerOptions: {
+    experimental: {
+      async: true
+    }
+  },
   preprocess: vitePreprocess()
 };
